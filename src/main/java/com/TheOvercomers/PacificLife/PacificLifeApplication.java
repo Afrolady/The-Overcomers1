bfcefad54ab0ae1957ec0f7799747123f4133ab3
Leydi Vidal <afroladymersi2012@gmail.com>
2022-08-17 11:22:37 -0500

    Proyecto Inicial

---
+++ src/main/java/com/TheOvercomers/PacificLife/PacificLifeApplication.java
@@ -0,0 +1,13 @@
+package com.TheOvercomers.PacificLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacificLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacificLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheOvercomers/PacificLife/PacificLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheOvercomers.PacificLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacificLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
