88a1e5abec9c428c3057a12f388943d4b388295b
Leydi Vidal <afroladymersi2012@gmail.com>
2022-08-18 08:51:52 -0500

    Hello World!

--- src/main/java/com/TheOvercomers/PacificLife/PacificLifeApplication.java
+++ src/main/java/com/TheOvercomers/PacificLife/PacificLifeApplication.java
@@ -2,9 +2,21 @@ package com.TheOvercomers.PacificLife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
+@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
 public class PacificLifeApplication {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello World!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PacificLifeApplication.class, args);
