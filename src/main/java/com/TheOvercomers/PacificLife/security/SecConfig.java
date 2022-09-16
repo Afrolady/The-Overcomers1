819b67a04bedde0d8576966b54eddfbf163e777f
Leydi Vidal <afroladymersi2012@gmail.com>
2022-09-16 10:50:24 -0500

    Se llevó a cabo el BASIC JDBC AUTHENTICATION A

--- src/main/java/com/TheOvercomers/PacificLife/security/SecConfig.java
+++ src/main/java/com/TheOvercomers/PacificLife/security/SecConfig.java
@@ -25,7 +25,7 @@ public class SecConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder()) //Va a la base de datos, desencripta la contraseña y despues compara ese valor con la contraseña que esta ingresando el usuario para log in.
                 .dataSource(dataSource) //esto es la continuacion de la linea de arriba pero como es muy larga se inicia aqui con punto
                 .usersByUsernameQuery("select correo,password,estado from empleado where correo=?") //Con este se puede sacar el nombre de usuario y la contraseña de aqui de esta Query /kwery/ Consulta .  Query es una consulta por una palabra clave o especifica
-                .authoritiesByUsernameQuery("select correo, rol from empleado where correo=?"); //Aqui verifica cual es mi rol en la empresa y que estoy autorizado a hacer con ese rol. (operativo tiene acceso restringido mientras que administrativo puede acceder a toda la informacion).
+                .authoritiesByUsernameQuery("select correo, rol from empleado where correo=?"); //Aqui verifica cual es mi rol en la empresa y que estoy autorizado a hacer con ese rol. (operativo tiene acceso restringido mientras que administrativo puede acceder a toda la informacion). De esta forma tanto los empleados como los jefes pueden determinar ciertas tareas
     }
 
 
